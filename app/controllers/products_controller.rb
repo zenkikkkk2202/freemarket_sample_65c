988fcc090710b940f06b9214056948da78442b4a
takei-teppei <boxmaikumanny348@gmail.com>
2019-12-19 18:11:17 +0900

    setting_routes_controller_view

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,50 @@
+class ProductsController < ApplicationController
+  def index
+    
+  end
+
+  def show
+    
+  end
+
+  def my_page
+    
+  end
+
+  def guide
+    
+  end
+
+  def notice
+  end
+
+  def things_to_do
+  end
+
+  def like
+  end
+
+  def exhibiting
+  end
+
+  def transaction
+  end
+
+  def sale
+  end
+
+  def purchase_exhibit
+  end
+
+  def purchase_transaction
+  end
+
+  def news
+  end
+
+  def evaluation
+  end
+
+  def contact
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "products#index"
+
+  resources :products, only: [:index,:show] do
+    collection do
+      get 'my_page'
+      get 'notice'
+      get 'things_to_do'
+      get 'like'
+      get 'exhibiting'
+      get 'transaction'
+      get 'sale'
+      get 'purchase_exhibit'
+      get 'purchase_transaction'
+      get 'news'
+      get 'evaluation'
+      get 'contact'
+      get 'guide'
+    end
+  end
 end
