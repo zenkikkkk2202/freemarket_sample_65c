2a43659dd689631e2bc70fbcd70e09c3d19fca05
takei-teppei <boxmaikumanny348@gmail.com>
2019-12-25 10:02:25 +0900

    setting_new

---
+++ app/assets/javascripts/product_new.js
@@ -0,0 +1,26 @@
+$(function(){
+  $('#price').change(function(){
+    var value = $(this).val();
+    if(value > 9999999){
+      $('#fee').val('-');
+    } else if(300 > value){
+      $('#fee').val('-');
+    } else{
+      var fee = value / 10;
+      $('#fee').val(fee);
+    }
+  });
+
+  $('#price').change(function(){
+    var value = $(this).val();
+    if(value > 9999999){
+      $('#profit').val('-');
+    } else if(300 > value){
+      $('#profit').val('-');
+    } else{
+      var fee = value / 10;
+      var profit = value - fee;
+      $('#profit').val(profit);
+    }
+  });
+});
