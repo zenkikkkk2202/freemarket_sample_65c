15eb2f335fa6fda98901d243c3c4d06688c2b751
yukimuneee <norugbynolife9@icloud.com>
2019-12-26 19:02:31 +0900

    setting

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,23 +5,20 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
          
   has_many :products
-  has_many :credit_cards
-  has_many :addresses
+  has_many :credit_cards, dependent: :destroy
+  has_many :addresses, dependent: :destroy
   belongs_to :facebook
   belongs_to :google
-  has_one :address, dependent: :destroy
-  has_one  :credit_card, dependent: :destroy
 
 
 
   # 各項目のバリデーション
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  PASSWORD_VALIDATION = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{7,128}+\z/i
+  PASSWORD_VALIDATION = /\A[a-z\d]{7,128}+\z/i
 
   validates :nickname,              presence: true, length: {maximum: 20}
   validates :email,                 presence: true, uniqueness: { case_sensitive: false }, format: { with: VALID_EMAIL_REGEX }
   validates :password,              presence: true, length: {minimum: 7, maximum: 128},    format: { with: PASSWORD_VALIDATION }
-  validates :password_confirmation, presence: true, length: {minimum: 7, maximum: 128}
   validates :name_family,           presence: true, length: {maximum: 35}
   validates :name_last,             presence: true, length: {maximum: 35}
   validates :name_kana_f,           presence: true, length: {maximum: 35}
