74458475f24a1e6f57582c0726087c26941a8cdf
takei-teppei <boxmaikumanny348@gmail.com>
2019-12-29 17:42:00 +0900

    setting

--- app/models/address.rb
+++ app/models/address.rb
@@ -9,7 +9,7 @@ class Address < ApplicationRecord
   POST_CODE_VALID = /\A\d{3}-\d{4}\z/i
   # バリデーション
   validates :post_code,               presence: true, length: {maximum: 30}, format: { with: POST_CODE_VALID }
-  validates :prefecture_id,           presence: true,
+  validates :prefecture_id,           presence: true
   validates :city,                    presence: true, length: {maximum: 50}
   validates :address,                 presence: true, length: {maximum: 50}
   validates :phone_number,            length: {maximum: 30}, uniqueness: { case_sensitive: false }
