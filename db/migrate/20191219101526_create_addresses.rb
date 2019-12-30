0943ffa8f8cb1a93d2b78ce531c2d2b4a40ca5ab
fujisyo <kinggggsyot@gmail.com>
2019-12-30 15:36:13 +0900

    setting_model_address

--- db/migrate/20191219101526_create_addresses.rb
+++ db/migrate/20191219101526_create_addresses.rb
@@ -1,7 +1,7 @@
 class CreateAddresses < ActiveRecord::Migration[5.2]
   def change
     create_table :addresses do |t|
-      t.integer :post_code, null: false
+      t.string :post_code, null: false
       t.integer :prefecture_id, null: false
       t.string :city, null: false
       t.string :address, null: false
