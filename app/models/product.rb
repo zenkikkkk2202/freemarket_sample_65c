eed3f49ca0eeec7a578ee720be53bc360d7f3532
takei-teppei <boxmaikumanny348@gmail.com>
2020-01-03 21:50:04 +0900

    setting_controller

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,9 +3,9 @@ class Product < ApplicationRecord
   
   belongs_to_active_hash :prefecture
   belongs_to :user, optional: true
-  belongs_to :saler, class_name: "User", optional: true, foreign_key: 'user_id'
-  belongs_to :buyer, class_name: "User", optional: true, foreign_key: 'user_id'
-  belongs_to :auction, class_name: "User", optional: true, foreign_key: 'user_id'
+  # belongs_to :saler, class_name: "User", optional: true, foreign_key: 'user_id'
+  # belongs_to :buyer, class_name: "User", optional: true, foreign_key: 'user_id'
+  # belongs_to :auction, class_name: "User", optional: true, foreign_key: 'user_id'
   has_many :product_images, dependent: :destroy
   accepts_nested_attributes_for :product_images, allow_destroy: true
 end
