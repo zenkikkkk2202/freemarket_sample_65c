23bf15695580abc3f65093c939203ca9a2bc5b68
takei-teppei <boxmaikumanny348@gmail.com>
2020-01-03 10:41:29 +0900

    setting
    コンフリクトエラーがあり。

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,89 @@
+class ProductsController < ApplicationController
+  def index
+    @products = Product.all.includes(:product_images)
+  end
+
+  def new
+    @product = Product.new
+    @product.product_images.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+  
+  
+  def show
+  end
+
+  def user_credit
+  end
+
+  def user_profile_information
+  end
+
+  def user_profile_edit
+  end
+
+  def user_logout
+  end
+
+  def user_page
+    
+  end
+
+  def guide
+    
+  end
+
+  def notice
+  end
+
+  def things_to_do
+  end
+
+  def like
+  end
+
+  def exhibiting
+  end
+
+  def transaction
+  end
+
+  def sale
+  end
+
+  def purchase_exhibit
+  end
+
+  def purchase_transaction
+  end
+
+  def news
+  end
+
+  def evaluation
+  end
+
+  def contact
+  end
+
+  def buy_confirm
+  end
+
+  private
+
+  def prefecture_params
+    params.require(:product).permit(:prefecture)
+  end
+
+  def product_params
+    params.require(:product).permit(:name,:description,:category,:condition,:charge,:prefecture_id,:day,:price,:fee,:profit, product_images_attributes: [:image, :_destroy])
+  end
+end
