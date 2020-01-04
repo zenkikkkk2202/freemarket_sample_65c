ce2a5ebf689968d23fb65cbe84659ee04c016e12
yukimuneee <norugbynolife9@icloud.com>
2020-01-04 17:23:18 +0900

    setting

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,54 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-        #  :omniauthable, omniauth_providers: %i[facebook google_oauth2]
+  #        :omniauthable, omniauth_providers: %i[facebook google_oauth2]
+
+  # def self.without_sns_data(auth)
+  #   user = User.where(email: auth.info.email).first
+
+  #     if user.present?
+  #       sns = SnsCredential.create(
+  #         uid: auth.uid,
+  #         provider: auth.provider,
+  #         user_id: user.id
+  #       )
+  #     else
+  #       user = User.new(
+  #         nickname: auth.info.name,
+  #         email: auth.info.email,
+  #       )
+  #       sns = SnsCredential.new(
+  #         uid: auth.uid,
+  #         provider: auth.provider
+  #       )
+  #     end
+  #     return { user: user ,sns: sns}
+  #   end
+
+  #   def self.with_sns_data(auth, snscredential)
+  #   user = User.where(id: snscredential.user_id).first
+  #   unless user.present?
+  #     user = User.new(
+  #       nickname: auth.info.name,
+  #       email: auth.info.email,
+  #     )
+  #   end
+  #   return {user: user}
+  #   end
+
+  #   def self.find_oauth(auth)
+  #   uid = auth.uid
+  #   provider = auth.provider
+  #   snscredential = SnsCredential.where(uid: uid, provider: provider).first
+  #   if snscredential.present?
+  #     user = with_sns_data(auth, snscredential)[:user]
+  #     sns = snscredential
+  #   else
+  #     user = without_sns_data(auth)[:user]
+  #     sns = without_sns_data(auth)[:sns]
+  #   end
+  #   return { user: user ,sns: sns}
+  # end
 
   has_many :products
   
