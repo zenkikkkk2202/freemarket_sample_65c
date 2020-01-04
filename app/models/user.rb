54c865afba0654cec5170dde32d2a8f9a5c5c613
takei-teppei <boxmaikumanny348@gmail.com>
2020-01-04 16:42:18 +0900

    setting_migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,9 +10,9 @@ class User < ApplicationRecord
   has_many :products
   
   has_many :credit_cards, dependent: :destroy
-  # has_many :saling_items, -> { where("buyer_id is NULL") }, foreign_key: "saler_id", class_name: "Product"
-  # has_many :sold_items, -> { where("buyer_id is not NULL") }, foreign_key: "saler_id", class_name: "Product"
-  # has_many :auction_items, -> { where("auction_id is not NULL") }, foreign_key: "saler_id", class_name: "Product"
+  has_many :saling_items, -> { where("buyer_id is NULL") }, foreign_key: "saler_id", class_name: "Product"
+  has_many :sold_items, -> { where("buyer_id is not NULL") }, foreign_key: "saler_id", class_name: "Product"
+  has_many :auction_items, -> { where("auction_id is not NULL") }, foreign_key: "saler_id", class_name: "Product"
   has_one :address, dependent: :destroy
 
 
--- db/migrate/20191219101326_create_products.rb
+++ db/migrate/20191219101326_create_products.rb
@@ -12,9 +12,9 @@ class CreateProducts < ActiveRecord::Migration[5.2]
       t.integer :fee, null: false
       t.integer :profit, null: false
       t.references :user, foreign_key: true
-      # t.references :saler, foreign_key: true
-      # t.references :buyer, foreign_key: true
-      # t.references :auction, foreign_key: true
+      t.integer :saler_id, foreign_key: true, comment:"user"
+      t.integer :buyer_id, foreign_key: true, comment:"user"
+      t.integer :auction_id, foreign_key: true, comment:"user"
       t.timestamps
     end
   end
