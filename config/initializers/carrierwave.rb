638ffb1b180a03b8fdb6bb80b0ae4af01903b20a
takei-teppei <boxmaikumanny348@gmail.com>
2020-01-06 02:55:55 +0900

    setting_deploy

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,19 +3,25 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-  if Rails.env.development? || Rails.env.test? 
-    config.storage = :file
-  elsif Rails.env.production?
-    config.storage = :fog
-  end
-  config.fog_provider = 'fog/aws'
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
-    aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
-    region: 'ap-northeast-1'
-  }
+  # if Rails.env.development? || Rails.env.test? 
+  #   config.storage = :file
+  # elsif Rails.env.production?
+  #   config.storage = :fog
+  # end
+  if Rails.env.production?
+    config.cache_storage = :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
+      aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
+      region: 'ap-northeast-1'
+    }
 
-  config.fog_directory  = 'freemarketsample65c'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/freemarketsample65c'
+    config.fog_directory  = 'freemarketsample65c'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/freemarketsample65c'
+  else
+    config.storage :file
+    config.enable_processing = false if Rails.env.test?
+  end
 end
