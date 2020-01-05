3c98025a690b27ecf396c56a26793af59d1f72c3
yukimuneee <norugbynolife9@icloud.com>
2020-01-05 12:46:17 +0900

    add-sns

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     sessions:      'users/sessions',
-    # omniauth_callbacks: 'users/omniauth_callbacks'
+    omniauth_callbacks: 'users/omniauth_callbacks'
   }
   devise_scope :user do
     get 'index',to:'users/registrations#index'
