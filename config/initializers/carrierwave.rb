22ffc0202f7e395087ac97adb930df074c57681e
takei-teppei <boxmaikumanny348@gmail.com>
2020-01-06 10:42:36 +0900

    setting_controller

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -21,7 +21,7 @@ CarrierWave.configure do |config|
     config.fog_directory  = 'freemarketsample65c'
     config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/freemarketsample65c'
   else
-    config.storage :file
+    config.storage = :file
     config.enable_processing = false if Rails.env.test?
   end
 end
