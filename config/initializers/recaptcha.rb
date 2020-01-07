cb9676e5e170a589aae1574f97227b7bb5157d59
yukimuneee <norugbynolife9@icloud.com>
2020-01-07 19:29:21 +0900

    setting

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,4 +1,4 @@
 Recaptcha.configure do |config|
-  config.site_key = Rails.application.credentials.recapcha['recapcha_public_key']
-  config.secret_key = Rails.application.credentials.recapcha['recapcha_private_key']
+  config.site_key = Rails.application.credentials.recapcha[:recapcha_public_key]
+  config.secret_key = Rails.application.credentials.recapcha[:recapcha_private_key]
 end
