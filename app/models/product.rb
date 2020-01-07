b05b022eafa9975eb6e59e9cecc9ec87e67ce643
fujisyo <kinggggsyot@gmail.com>
2020-01-07 15:14:02 +0900

    setting_asosiesyon

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  
+  belongs_to :user
   belongs_to_active_hash :prefecture
   belongs_to :saler, class_name: "User", optional: true,foreign_key: "saler_id"
   belongs_to :buyer, class_name: "User", optional: true,foreign_key: "buyer_id"
