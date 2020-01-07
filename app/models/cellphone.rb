5a406e1a7ede33c8f21c61e3535ff7c714e57919
yukimuneee <norugbynolife9@icloud.com>
2020-01-07 14:36:18 +0900

    setting

--- app/models/cellphone.rb
+++ app/models/cellphone.rb
@@ -1,3 +1,8 @@
 class Cellphone < ApplicationRecord
   belongs_to :user, optional: true
+
+  CELLPHONE_NUMBER_CODE_VALID = /\A(070|080|090)-\d{4}-\d{4}\z/
+  # バリデーション
+  validates :cellphone_number,            uniqueness: { case_sensitive: false }, format: { with: PHONE_NUMBER_CODE_VALID }
 end
+
