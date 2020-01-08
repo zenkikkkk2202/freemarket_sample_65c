c50fc73b59aaa9957146d3a0b40430a3bbe89cfa
yukimuneee <norugbynolife9@icloud.com>
2020-01-08 17:56:06 +0900

    setting

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,7 @@ Rails.application.routes.draw do
 
   resources :products do
     collection do
+      get 'search'
       get 'user_credit'
       get 'user_profile_information'
       get 'user_logout'
