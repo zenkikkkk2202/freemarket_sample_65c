02da83d6852f1e2e6c5c10d528da0a0993f39cc5
takei-teppei <boxmaikumanny348@gmail.com>
2020-01-08 12:25:28 +0900

    setting_userpage_model

---
+++ db/migrate/20191219101326_create_products.rb
@@ -0,0 +1,21 @@
+class CreateProducts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :products do |t|
+      t.string :name, null: false
+      t.text :description, null: false
+      t.string :category, null: false, default: 0
+      t.string :condition, null: false, default: 0
+      t.string :charge, null: false, default: 0
+      t.string :prefecture_id, null: false, default: 0
+      t.string :day, null: false, default: 0
+      t.integer :price, null: false
+      t.integer :fee, null: false
+      t.integer :profit, null: false
+      t.references :user, foreign_key: true
+      t.references :saler, foreign_key: {to_table: :users} 
+      t.references :buyer, foreign_key: {to_table: :users}
+      t.references :auction, foreign_key: {to_table: :users}
+      t.timestamps
+    end
+  end
+end
